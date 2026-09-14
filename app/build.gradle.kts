2c2ec4cfd65f24a8a6d5c8b0155b9ec74a375091
江寒 <3685634009@qq.com>
2026-09-14 13:13:37 +0800

    build(app): 更新版本号到39和版本名称3.5.0
    完成本次版本的迭代版本号更新，准备发布3.5.0正式版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.yichao.evilgodxu"
         minSdk = 33
         targetSdk = 37
-        versionCode = 38
-        versionName = "3.4.3"
+        versionCode = 39
+        versionName = "3.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
