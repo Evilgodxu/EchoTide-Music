838384c122813f2e7f24f7cdce68a70390f7e224
江寒 <3685634009@qq.com>
2026-09-15 02:32:56 +0800

    build(app): 升级应用版本到3.5.1
    将versionCode从39更新为40，versionName更新为3.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.yichao.evilgodxu"
         minSdk = 33
         targetSdk = 37
-        versionCode = 39
-        versionName = "3.5.0"
+        versionCode = 40
+        versionName = "3.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
